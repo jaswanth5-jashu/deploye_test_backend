cfa28f476f7f633b998672340426e4fa0a1e3c30
Jaswanth Simha <jaswanthsimha2004@gmail.com>
2026-01-29 16:12:18 +0530

    it uup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,16 +62,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 # ---------------- DATABASE ----------------
+import os
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "project_cits",
-        "USER": "root",
-        "PASSWORD": "Jashu@2004",
-        "HOST": "localhost",  
-        "PORT": "3306",
+        "NAME": os.environ.get("MYSQLDATABASE"),
+        "USER": os.environ.get("MYSQLUSER"),
+        "PASSWORD": os.environ.get("MYSQLPASSWORD"),
+        "HOST": os.environ.get("MYSQLHOST"),
+        "PORT": os.environ.get("MYSQLPORT"),
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 # ---------------- EMAIL ----------------
